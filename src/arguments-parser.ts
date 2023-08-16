622a89f3eecfee80ae0c765d55abac4627129fa3
Md. Shahadul Alam Patwary <shahadul_alam_17@outlook.com>
2023-08-16 16:06:40 +0600

    Arguments parser updated

--- src/arguments-parser.ts
+++ src/arguments-parser.ts
@@ -1,10 +1,11 @@
+import { IMap, Map, } from "@shahadul-17/collections";
 import { StringUtilities } from "./string-utilities";
 
 const ARGUMENT_NAME_PREFIX = "--";
 
 export class ArgumentsParser {
 
-  private static readonly argumentsMap: Record<string, string>
+  private static readonly argumentsMap: IMap<string, string>
     = this.populateArgumentsMap(process.argv);
 
   /**
@@ -12,8 +13,8 @@ export class ArgumentsParser {
    * @param args Arguments that shall be used to populate map.
    * @returns Returns a map containing arguments.
    */
-  private static populateArgumentsMap(args: Array<string>): Record<string, string> {
-    const argumentsMap: Record<string, string> = Object.create(null);
+  private static populateArgumentsMap(args: Array<string>): IMap<string, string> {
+    const argumentsMap: IMap<string, string> = new Map<string, string>();
 
     for (let i = 2; i < args.length; ++i) {
       const argument = args[i];
@@ -25,7 +26,7 @@ export class ArgumentsParser {
       const argumentName = argument.substring(2);
       const argumentValue = args[i];        // next argument is the value...
 
-      argumentsMap[argumentName] = argumentValue;
+      argumentsMap.set(argumentName, argumentValue);
     }
 
     return argumentsMap;
@@ -37,6 +38,19 @@ export class ArgumentsParser {
    * @returns Command-line argument value.
    */
   public static getArgument(argumentName: string): string {
-    return StringUtilities.getDefaultIfUndefinedOrNull(this.argumentsMap[argumentName]);
+    const argumentValue = this.argumentsMap.get(argumentName);
+
+    return StringUtilities.getDefaultIfUndefinedOrNull(argumentValue);
+  }
+
+  public static getArguments(): Record<string, string> {
+    const entries = this.argumentsMap.entries();
+    const _arguments: Record<string, string> = Object.create(null);
+
+    for (const { key, value, } of entries) {
+      _arguments[key] = value;
+    }
+
+    return _arguments;
   }
 }
